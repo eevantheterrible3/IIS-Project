6532fbf92623a648d4a9de904474fb4bff364adf
ogriz <uros.ogrizovic@gmail.com>
2026-05-26 15:21:28 +0200

    Add CRUD operations for DocumentRating entity

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ from routers.documents import router as documents_router
 from routers.document_types import router as document_types_router
 from routers.section_templates import router as section_templates_router
 from routers.document_sections import router as document_sections_router
+from routers.document_ratings import router as document_ratings_router
 
 app = FastAPI(title="IIS API")
 app.include_router(auth_router)
@@ -15,6 +16,7 @@ app.include_router(documents_router)
 app.include_router(document_types_router)
 app.include_router(section_templates_router)
 app.include_router(document_sections_router)
+app.include_router(document_ratings_router)
 
 app.add_middleware(
     CORSMiddleware,
